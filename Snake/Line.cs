926b7b1a68fe571332245452543a8ea6e421b114
FullSilence <HromovValentin@gmail.com>
2016-09-10 18:06:10 +0300

    2

--- Snake/Line.cs
+++ Snake/Line.cs
@@ -1,43 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Snake
+﻿namespace Snake
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-
-            new LineHorisontal(3, 35, 8, '*').draw();
-            new LineVertical(6, 20, 3, '#').draw();
-
-            Console.ReadKey();
-        }
-
-    }
-
-    class Point
-    {
-        public int x;
-        public int y;
-        public char symbol;
-
-        public Point(int _x, int _y, char _symbol = '*')
-        {
-            x = _x;
-            y = _y;
-            symbol = _symbol;
-        }
-
-        public void draw()
-        {
-            Console.SetCursorPosition(x, y);
-            Console.Write(symbol);
-        }
-    }
+    using System.Collections.Generic;
 
     class Line
     {
