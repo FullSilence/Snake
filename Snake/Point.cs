1c2db998e540519485a26270c0c5853498198506
FullSilence <HromovValentin@gmail.com>
2016-09-10 18:08:32 +0300

    2

---
+++ Snake/Point.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake
+{
+    class Point
+    {
+        public int x;
+        public int y;
+        public char symbol;
+
+        public Point(int _x, int _y, char _symbol = '*')
+        {
+            x = _x;
+            y = _y;
+            symbol = _symbol;
+        }
+
+        public void draw()
+        {
+            Console.SetCursorPosition(x, y);
+            Console.Write(symbol);
+        }
+    }
+}
