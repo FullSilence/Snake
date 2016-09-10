1cdcf09d22a9ed1c701f423128ca2fc32b61e48d
FullSilence <HromovValentin@gmail.com>
2016-09-10 15:14:35 +0300

    Lesson1

---
+++ Snake/Point.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Snake
+{
+    class Point
+    {
+        public int x;
+        public int y;
+        public char symbol;
+
+        public Point(int _x, int _y, char _symbol = '*')
+        {
+            x = _x;
+            y = _y;
+            symbol = _symbol;
+        }
+
+        public void draw()
+        {
+            Console.SetCursorPosition(x, y);
+            Console.Write(symbol);
+        }
+    }
+}
--- Snake/Program.cs
+++ Snake/Program.cs
@@ -11,30 +11,10 @@ namespace Snake
         static void Main(string[] args)
         {
 
-            new point(5, 3).draw();
+            new Point(5, 3).draw();
 
             Console.ReadKey();
         }
 
-    }
-
-    class point
-    {
-        public int x;
-        public int y;
-        public char symbol;
-
-        public point(int _x, int _y, char _symbol = '*')
-        {
-            x = _x;
-            y = _y;
-            symbol = _symbol;
-        }
-
-        public void draw()
-        {
-            Console.SetCursorPosition(x, y);
-            Console.Write(symbol);
-        }
     }
 }
